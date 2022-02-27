d760b3c2cd70ecc47c42dc8057c8c421968b81d3
stelios <steliosks@protonmail.com>
2022-02-27 20:02:19 +0100

    Added main's basic structure

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,17 @@
 using namespace std;
 
 int main(){
+    cout << "To exit, hit 'X' and then Enter." << endl;
+    while(true){
+        cout << "Enter prompt: " ;
+        string input;
+        getline(cin,input);
+        cout << input << endl;
 
-}
+        if(input == "X"){
+            break;
+        }else{
+        //TODO
+        }
+    }    
+} 
